2c80c7995f1353a70de1b4e77708a655f7c48c18
ladeeluc <laura.hatch.lh@gmail.com>
2019-10-12 17:58:36 -0500

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,5 +1,7 @@
-# Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
-#require 'pry'
+# Examples inspired by David Foster Wallace's 
+# (2/21/1962 to 9/12/2008) _Infinite Jest_
+
+require 'pry'
 
 ENNET_HOUSE = [
   "Don Gately",
@@ -23,22 +25,22 @@ def assembled_matrix
     ENFIELD_TENNIS_ACADEMY
 end
   
-#new_array = []
+
 def array_literal_matrix
-    residents.new = []
-    residents.new << residents.sort{ |a,z| a <=> z}
-  #binding.pry
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
-  # alphabetically by the first character.
+  [ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort]
 end
 
 
-# #def matrix_lookup(matrix, row, column)
-#   # Return the matrix's content at row and column
-# end
+def matrix_lookup(matrix, row, column)
+  matrix[1][1]
+  # Return the matrix's content at row and column
+  binding.pry
+end
 
-# def matrix_update(matrix, row, column, new_value)
-#   # Update the matrix location at row and column to have the value of new_value
-#   # Return the updated matrix
-# end
+def matrix_update(matrix, row, column, new_value)
+   matrix = []
+  # new_value << matrix[0][2]
+  # Update the matrix location at row and column to have the value of new_value
+  # Return the updated matrix
+end
+new_value
