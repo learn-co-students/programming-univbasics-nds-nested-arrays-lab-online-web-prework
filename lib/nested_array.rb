35d5edbfefd167119889ab937e18bb6d3adba73b
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-10-25 17:01:30 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,21 +17,40 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-  # Build an array that contains (or, "nests") the residents of The Ennet House
-  # and the Enfield Tennis Academy as provided by the constants
+  assembled_matrix=[
+    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
+    ["Hal Incandenza", "Lyle", "Gerhard Schtitt",  "Mario Incandenza",  "Michael Pemulis"]
+  ]
 end
 
 def array_literal_matrix
+  array_literal_matrix=   [
+      ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"].sort,
+      ["Hal Incandenza", "Lyle", "Gerhard Schtitt",  "Mario Incandenza",  "Michael Pemulis"].sort
+       ]
+array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
 end
 
 def matrix_lookup(matrix, row, column)
+  matrix_lookup= [
+
+    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
+    ["Hal Incandenza", "Lyle", "Gerhard Schtitt",  "Mario Incandenza",  "Michael Pemulis"]
+  ]
+  matrix_lookup[1][1]
   # Return the matrix's content at row and column
 end
 
 def matrix_update(matrix, row, column, new_value)
+  matrix_update=[
+    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
+    ["Hal Incandenza", "Lyle", "Gerhard Schtitt",  "Mario Incandenza",  "Michael Pemulis"]
+  ]
+  matrix_update[0][2]= "Tiny Ewell"
+matrix_update
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
 end
