e886c0a693b0c7433e9b39703520661f23ac686f
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-06 00:25:50 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,19 +19,86 @@ ENFIELD_TENNIS_ACADEMY = [
 def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
+ENNET_HOUSE = [
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
+]
+ENFIELD_TENNIS_ACADEMY = [
+  "Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"
+]
+  ENNET_HOUSE_WITH_ACADEMY= [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
 end
 
 def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
+  ENNET_HOUSE = [
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
+]
+ENFIELD_TENNIS_ACADEMY = [
+  "Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"
+]
+  ENNET_HOUSE_WITH_ACADEMY= [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  puts ENNET_HOUSE_WITH_ACADEMY.sort
 end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
+    ENNET_HOUSE = [
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
+]
+ENFIELD_TENNIS_ACADEMY = [
+  "Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"
+]
+  
+  ENNET_HOUSE_WITH_ACADEMY= [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  ENNET_HOUSE[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
+      ENNET_HOUSE = [
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
+]
+ENFIELD_TENNIS_ACADEMY = [
+  "Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"
+]
+  
+  ENNET_HOUSE_WITH_ACADEMY= [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  ENNET_HOUSE[0][1]
+  ENNET_HOUSE[0][1] = "Charlie"
+  ENNET_HOUSE[0][1]
 end
