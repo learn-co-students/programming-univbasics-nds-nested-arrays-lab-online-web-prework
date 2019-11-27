af97b79abb32dd77fa9f12326c42771c103b3f37
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-27 05:01:42 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,13 +17,28 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-  # Build an array that contains (or, "nests") the residents of The Ennet House
-  # and the Enfield Tennis Academy as provided by the constants
+  ENNET_HOUSE=["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce green"]
+  ENFIELD_TENNIS_ACADEMY=["Hal Incandenza", "Lyle", "Gerhard Schtitt",
+  "Mario Incandenza","Michael Pemulis"]
+  
+  Tennis_Players=[
+    ENNET_HOUSE,
+    ENFIELD_TENNIS_ACADEMY,
+    ]#=>[["Don Gately", "Joelle van Dyne","Pat M.","Kate Gompert","Bruce Green", "Hal Incandenza", "Lyle", "Gerhard Schtitt","Mario Incandenza", "Michael Pemulis"]
 end
 
 def array_literal_matrix
-  # Using Array literal syntax only, build a nested array that uses the data in
+  ENNET_HOUSE=["Bruce Green", "Don Gately","Joelle van Dyne","Kate Gompert","Pat M."]# Using Array literal syntax only, build a nested array that uses the data in
+  ENFIELD_TENNIS_ACADEMY=["Gerhard Schtitt", "Hal Incandenza","Lyle","Mario Incandenza","Michael Pemulis"
+  
+  ]#=>[["Bruce Green", "Don Gately",Gerhard Schtitt","Hal Incandenza","Joelle van Dyne","Kate Gompert","Lyle","Mario Incandenza","Michael Pemulis","Pat M."]
+  
+  
+  
+  
+  
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
+  
   # alphabetically by the first character.
 end
 
