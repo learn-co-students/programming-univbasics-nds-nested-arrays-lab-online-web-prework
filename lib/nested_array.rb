87030d25ddfe68532333b150b552b37133f194b8
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-08 21:00:57 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,25 +17,40 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  # Build an array that contains both of the above arrays
-  # This matrix will represent a produce storage room
-  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
-  # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
+  newarr = [
+    ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
+    ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
+    
+    ]
+  return newarr
 end
 
 def sorted_matrix
+  
+  newarr = [
+    ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
+    ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
+    
+    ]
+    
+    newarr[0] = newarr[0].sort 
+    newarr[1] = newarr[1].sort 
+    
+    return newarr
+    
+    
+  
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+  return matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
+  matrix[row][column] = new_value
+  
+  return matrix
 end
